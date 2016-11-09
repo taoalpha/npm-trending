a55606f1e2a449e40317da2cc4b7477045ce4fa8
TaoAlpha <iamzhoutao92@gmail.com>
2016-11-08 20:58:06 -0800

    set up a simplest args parser, optimize the writeFile with sync and debounce

--- analyze.js
+++ analyze.js
@@ -8,10 +8,15 @@
 
 const ds = require('./lib/ds');
 
-ds.read('./data/db.json');
-console.log(ds.getTop(10, 'stats.dayInc').map(v => v.name));
-console.log(ds.getTop(10, 'stats.dayChange').map(v => v.name));
-console.log(ds.getTop(10, 'stats.nowInc').map(v => v.name));
-console.log(ds.getTop(10, 'stats.nowChange').map(v => v.name));
+let dat = ds.read('./data/db.json');
+console.log(ds.getTop(10, 'stats.dayInc'));
+console.log(ds.getTop(10, 'stats.dayChange'));
+console.log(ds.getTop(10, 'stats.nowInc'));
+console.log(ds.getTop(10, 'stats.nowChange'));
+
+console.log(ds.getTop(10, 'stats.dayInc').map(v => v.name).join(','));
+console.log(ds.getTop(10, 'stats.dayChange').map(v => v.name).join(','));
+console.log(ds.getTop(10, 'stats.nowInc').map(v => v.name).join(','));
+console.log(ds.getTop(10, 'stats.nowChange').map(v => v.name).join(','));
 const d = new Date();
 d.setTime(d.getTime() - (d.getTimezoneOffset() * 60 * 1000));
