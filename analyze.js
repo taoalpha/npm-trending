d83837d507caf6e2654ff9e0406955cfc63905eb
TaoAlpha <iamzhoutao92@gmail.com>
2016-11-10 00:07:18 -0800

    update data, add read as helper, support fetch info as separate file

--- analyze.js
+++ analyze.js
@@ -9,10 +9,13 @@
 const ds = require('./lib/ds');
 
 let dat = ds.read('./data/db.json');
-console.log(ds.getTop(10, 'stats.dayInc'));
-console.log(ds.getTop(10, 'stats.dayChange'));
-console.log(ds.getTop(10, 'stats.nowInc'));
-console.log(ds.getTop(10, 'stats.nowChange'));
+console.log(ds.getTop(10, 'stats.dayInc').map(v => {return {name: v.name, dayInc: v.stats.dayInc}}));
+console.log(ds.getTop(10, 'stats.dayChange').map(v => {return {name: v.name, dayChange: v.stats.dayChange}}));
+console.log(ds.getTop(10, 'stats.nowInc').map(v => {return {name: v.name, nowInc: v.stats.nowInc}}));
+console.log(ds.getTop(10, 'stats.nowChange').map(v => {return {name: v.name, nowChange: v.stats.nowChange}}));
+
+console.log(ds.getBottom(10, 'fetchTime').map(v => {return {name: v.name, fetchTime: v.fetchTime, fetchTimePretty: v.fetchTimePretty}}));
+console.log(ds.getBottom(10, 'createTime').map(v => {return {name: v.name, createTime: v.createTime, createTimePretty: v.createTimePretty}}));
 
 console.log(ds.getTop(10, 'stats.dayInc').map(v => v.name).join(','));
 console.log(ds.getTop(10, 'stats.dayChange').map(v => v.name).join(','));
