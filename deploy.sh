36a930cac2364935e9b5b739dc6f5fe10792ad90
taoalpha <iamzhoutao92@gmail.com>
2016-11-12 23:22:46 -0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -139,7 +139,7 @@ main() {
 initial_deploy() {
 	git --work-tree "$deploy_directory" checkout --orphan $deploy_branch
 	git --work-tree "$deploy_directory" add --all
-	commit+push
+	commit_push
 }
 
 incremental_deploy() {
@@ -154,7 +154,7 @@ incremental_deploy() {
 	set -o errexit
 	case $diff in
 		0) echo No changes to files in $deploy_directory. Skipping commit.;;
-		1) commit+push;;
+		1) commit_push;;
 		*)
 			echo git diff exited with code $diff. Aborting. Staying on branch $deploy_branch so you can debug. To switch back to master, use: git symbolic-ref HEAD refs/heads/master && git reset --mixed >&2
 			return $diff
@@ -162,7 +162,7 @@ incremental_deploy() {
 	esac
 }
 
-commit+push() {
+commit_push() {
 	set_user_id
 	git --work-tree "$deploy_directory" commit -m "$commit_message"
 
@@ -210,10 +210,6 @@ restore_head() {
 
 filter() {
 	sed -e "s|$repo|\$repo|g"
-}
-
-sanitize() {
-	"$@" 2> >(filter 1>&2) | filter
 }
 
 [[ $1 = --source-only ]] || main "$@"
